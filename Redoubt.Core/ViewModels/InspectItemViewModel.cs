a5b8bbf7a3724b0895d9bf510ed843e2b03910a8
Steven Confessore <steven.confessore@gmail.com>
2019-02-05 00:36:28 -0500

    lots of work on displaying inventory

---
+++ Redoubt.Core/ViewModels/InspectItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Core.ViewModels;
+using System.Windows.Input;
+
+namespace Redoubt.Core.ViewModels
+{
+    public class InspectItemViewModel : MvxViewModel<Item>
+    {
+        public Item Item { get; set; } = new Item();
+
+        public ICommand NavBack
+        {
+            get => new MvxCommand(() => Close(this));
+        }
+
+        public override void Prepare(Item item)
+        {
+            Item = item;
+        }
+    }
+}
--- Redoubt.Core/ViewModels/InventoryViewModel.cs
+++ Redoubt.Core/ViewModels/InventoryViewModel.cs
@@ -1,19 +1,31 @@
-﻿using MvvmCross.Core.ViewModels;
-using System.Collections.Generic;
+﻿using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
 using System.Windows.Input;
 
 namespace Redoubt.Core.ViewModels
 {
     public class InventoryViewModel : MvxViewModel
     {
+        readonly IMvxNavigationService nav;
+
+        public InventoryViewModel(IMvxNavigationService nav)
+        {
+            this.nav = nav;
+        }
+        
         public ICommand NavBack
         {
             get => new MvxCommand(() => Close(this));
         }
 
-        public List<Item> Inventory
+        public MvxObservableCollection<Item> Inventory
         {
-            get => App.Player.Inventory;
+            get => new MvxObservableCollection<Item>(App.Player.Inventory);
+        }
+
+        public ICommand Inspect
+        {
+            get => new MvxCommand<Item>(x => nav.Navigate<InspectItemViewModel, Item>(x));
         }
     }
 }
